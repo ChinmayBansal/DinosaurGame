ee79c9e2507eb51d0a52e669ecf3e65c417e1044
Chinmay Bansal <chinmaybansalsc@outlook.com>
2023-01-18 18:16:01 -0500

    Initial Commit

---
+++ DGStarter/Dinosaur.swift
@@ -0,0 +1,21 @@
+//
+//  Dinosaur.swift
+//  DGStarter
+//
+//  Created by Chinmay Bansal on 01/28/23.
+//
+
+import UIKit
+
+// This struct will model the properties for a Dinosaur
+struct Dinosaur {
+    let name: String
+    let image: UIImage
+    let type: String
+    let weight: Double
+    let height: Double
+    let diet: String
+    let era: String
+    let region: String
+    let speed: Int
+}
